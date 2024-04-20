e0a8c4c2c07cd0eb05270425eec61b14e3cb3557
ShawnZhuPro <shawnz05827@gmail.com>
2024-04-20 12:35:26 -0700

    Parsing and image works, set dimensions works

--- Image Revealer Game/src/application/Main.java
+++ Image Revealer Game/src/application/Main.java
@@ -1,43 +1,40 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	Image image;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Button btn = new Button();
-	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
-	        //
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
+	public void start(Stage stage) {
+		image = new Image("file:./src/Images/koala.png");
+		ImageView imageView = new ImageView();
+		
+		// Set the dimensions of the ImageView
+        imageView.setFitWidth(800);  // Set the desired width
+        imageView.setFitHeight(600); // Set the desired height
+        imageView.setPreserveRatio(true); // Maintain aspect ratio
+        imageView.setImage(image);
 
-	 Scene scene = new Scene(root, 300, 250);
+	    //Setting the Scene object
+		// Set the size of the scene to match the image dimensions
+        Scene scene = new Scene(new Group(imageView), image.getWidth(), image.getHeight());
+	    stage.setTitle("Displaying Image");
+	    stage.setScene(scene);
+	    stage.show();
+		
 
-	        primaryStage.setTitle("Hello World!");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
