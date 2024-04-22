555e5e578b0f8372a8c2833bbdc0393f94040133
1905827 <1905827@01-19785.pusd.dom>
2024-04-22 11:38:23 -0700

    Basic logic to create circles

--- Image Revealer Game/src/application/Main.java
+++ Image Revealer Game/src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 
@@ -24,10 +25,12 @@ public class Main extends Application {
 
 	    //Setting the Scene object
 		// Set the size of the scene to match the image dimensions
-        Scene scene = new Scene(new Group(imageView), image.getWidth(), image.getHeight());
-	    stage.setTitle("Displaying Image");
-	    stage.setScene(scene);
-	    stage.show();
+//        Scene scene = new Scene(new Group(imageView), image.getWidth(), image.getHeight());
+//	    stage.setTitle("Displaying Image");
+//	    stage.setScene(scene);
+//	    stage.show();
+        
+        splitCircle(stage);
 	}
 	
 	// Method to calculate the average of all pixel colors
@@ -36,7 +39,33 @@ public class Main extends Application {
 	}
 	
 	// Recursively split circles
-	public void splitCircle() {
+	public void splitCircle(Stage stage) {
+		//Drawing a Circle 
+	      Circle circle = new Circle(); 
+	         
+		 //Setting the properties of the circle 
+		 circle.setCenterX(300.0f); 
+		 circle.setCenterY(135.0f); 
+		 circle.setRadius(100.0f); 
+		     
+		 //Creating a Group object  
+		 Group root = new Group(circle); 
+		     
+		 //Creating a scene object 
+		 Scene scene = new Scene(root, 600, 300);  
+		 
+		 //Setting title to the Stage 
+		 stage.setTitle("Drawing a Circle"); 
+		     
+		 //Adding scene to the stage 
+		 stage.setScene(scene); 
+		 
+		 // Set stage width/height to the dimensions we want
+		 stage.setWidth(800);
+		 stage.setHeight(600);
+		     
+		 //Displaying the contents of the stage 
+		 stage.show();
 		
 	}
 	
