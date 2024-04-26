5d5d46a8384d445def92438b002abcc4815ad051
1905827 <1905827@01-19785.pusd.dom>
2024-04-26 11:36:53 -0700

    test commit

--- Image Revealer Game/src/application/Main.java
+++ Image Revealer Game/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Pane root = new Pane();
+        Pane root = new Pane();//
         Scene scene = new Scene(root, maxSize, maxSize);
         
         // Initialize image
