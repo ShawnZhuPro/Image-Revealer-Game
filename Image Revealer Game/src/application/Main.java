df5acb29852ef8f08798f7b23aec1e918c132c07
1905827 <1905827@01-19785.pusd.dom>
2024-04-29 11:41:01 -0700

    Get avg colors partially working (not accurate)

--- Image Revealer Game/src/application/Main.java
+++ Image Revealer Game/src/application/Main.java
@@ -24,12 +24,12 @@ public class Main extends Application {
         
         // Initialize image
         Image image = new Image("file:./src/Images/koala.png");
-
-        // Create initial big circle
-        int red = getR(image, 0, 0);
-    	int green = getR(image, 0, 0);
-    	int blue = getR(image, 0, 0);
-        Circle bigCircle = new Circle(maxSize / 2, maxSize / 2, maxSize / 4, Color.rgb(red,  green,  blue));
+        
+        // Initialize initial big circle
+        int width = (int) image.getWidth();
+        int height = (int) image.getHeight();
+        Color avgColor = extractAvg(image, 0, 0, width, height);
+        Circle bigCircle = new Circle(maxSize / 2, maxSize / 2, maxSize / 4, avgColor);
         root.getChildren().add(bigCircle);  // Adds bigCircle to the pane named "root" (basically displays the circle)
 
         // Add mouse event handler for splitting the big starter circle
@@ -39,7 +39,7 @@ public class Main extends Application {
             if (!bigCircle.getProperties().containsKey("split")) {
                 bigCircle.getProperties().put("split", true);
                 circlesSplit++;
-                splitCircle(image, bigCircle, 0);  // The entire algorithm is pretty much based off this
+                splitCircle(image, bigCircle, 0, 0, 0, width, height);
             }
         });
 
@@ -49,7 +49,7 @@ public class Main extends Application {
 
     }
 
-    private void splitCircle(Image image, Circle circle, int depth) {
+    private void splitCircle(Image image, Circle circle, int depth, int x0, int y0, int width, int height) {
     	// Base case
         if (depth >= targetDepth) {
             return;
@@ -60,89 +60,56 @@ public class Main extends Application {
         double newRadius = circle.getRadius() / 2;
         double x = circle.getCenterX();
         double y = circle.getCenterY();
-
+        
         // Remove the current circle
         circle.setFill(Color.TRANSPARENT);
+        
+        // X and Y directions for circles
+        int[] dx = {-1, 1, -1, 1};
+        int[] dy = {-1, -1, 1, 1};
 
         // Create and add 4 smaller circles
         for (int i = 0; i < 4; i++) {
-        	Circle smallerCircle = new Circle();
-        	// Update attributes of each circle
         	// Logic that positions each circle in its expected spots
-        	smallerCircle.setCenterX(x+(i%2==0 ? -1 : 1)*newRadius);
-        	smallerCircle.setCenterY(y+(i/2==0 ? -1 : 1)*newRadius);
-        	smallerCircle.setRadius(newRadius);
-        	int red = getR(image, depth, i);
-        	int green = getR(image, depth, i);
-        	int blue = getR(image, depth, i);
-        	Color color = Color.rgb(red, green, blue);   
+        	// Update attributes of each circle
+        	int newX = (int)(x + dx[i] * newRadius);
+        	int newY = (int)(y + dy[i] * newRadius);
+        	int newW = width /2;
+        	int newH = height / 2;
+        	Color newColor = extractAvg(image, x0 + (i%2) * newW, y0 + (i/2) * newH, newW, newH);
+        	// Initialize new circle
+        	Circle smallerCircle = new Circle(newX, newY, newRadius, newColor);
 
-        	smallerCircle.setFill(color);
-        	
         	// Draws the circle on the scene
         	((Pane) circle.getParent()).getChildren().add(smallerCircle);
 
+        	int iMod = i%2;
+        	int iDiv = i/2;
             // Mouse event handler for circle splitting
         	smallerCircle.setOnMouseEntered(event -> {
         		if(!smallerCircle.getProperties().containsKey("split")) {
         			smallerCircle.getProperties().put("split", true);
-        			splitCircle(image, smallerCircle, depth+1);
+        			splitCircle(image, smallerCircle, depth+1, x0 + (iMod) * newW, y0 + (iDiv) * newH, newW, newH);
         		}
         	});
             
         }
     }
     
-    // Gets the average R color value of the image//
-    private int getR(Image image, int depth, int i) {
-        PixelReader pixelReader = image.getPixelReader();
-        int height = (int) image.getHeight();
-        int width = (int) image.getWidth();
-        
-        return extractAvg(image, "red", height / (depth+1), width / (depth+1));
-    }
-    
-    private int getG(Image image, int depth, int i) {
-        PixelReader pixelReader = image.getPixelReader();
-        int height = (int) image.getHeight();
-        int width = (int) image.getWidth();
-        
-        return extractAvg(image, "green", height / (depth+1), width / (depth+1));
-    }
-    
-    private int getB(Image image, int depth, int i) {
-        PixelReader pixelReader = image.getPixelReader();
-        int height = (int) image.getHeight();
-        int width = (int) image.getWidth();
-        
-        return extractAvg(image, "blue", height / (depth+1), width / (depth+1));
-    }
-    
-    private int extractAvg(Image image, String color, int height, int width) {
-    	int sum = 100;
-    	int count = 0;
+    private Color extractAvg(Image image, int x0, int y0, int width, int height) {
     	PixelReader pixelReader = image.getPixelReader();
-    	for (int readY = 0; readY < height; readY++) {
-            for (int readX = 0; readX < width; readX++) {
-                Color c = pixelReader.getColor(readX, readY);
-                if(color.equals("red")) {
-            		sum += c.getRed();
-            	}
-            	else if(color.equals("green")) {
-            		sum += c.getGreen();
-            	} 
-            	else if(color.equals("blue")) {
-            		sum += c.getBlue();
-            	}
+    	int sumR = 0, sumG = 0, sumB = 0;
+    	int count = 0;
+    	for (int y = 0; y < y0 + height; y++) {
+            for (int x = 0; x < x0 + width; x++) {
+                Color c = pixelReader.getColor(x, y);
+                sumR += (long) (c.getRed() * 255);
+                sumG += (long) (c.getGreen() * 255);
+                sumB += (long) (c.getBlue() * 255);
                 count++;
             }
         }
-    	System.out.println("Sum" + sum);
-    	System.out.println("Count" + count);
-    	System.out.println("Color" + color);
-    	System.out.println("Height" + height);
-    	System.out.println("Width" + width);
-    	return sum/count;
+    	return Color.rgb((int) (sumR / count), (int) (sumG / count), (int) (sumB / count));
     }
     
 
