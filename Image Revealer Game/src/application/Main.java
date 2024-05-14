14a4cd5161d3ebc56b740081c7cf713092b9790f
1934673 <1934673@01-19850.pusd.dom>
2024-05-14 11:40:07 -0700

    Added all the image folders, added randomness functionality to loading the images, and added a back button

--- Image Revealer Game/src/application/Main.java
+++ Image Revealer Game/src/application/Main.java
@@ -28,6 +28,18 @@ public class Main extends Application {
 	 */
 	@Override
 	public void start(Stage stage) {
+		Button backButton = new Button("Go Back");
+		backButton.setLayoutY(900);
+		backButton.setLayoutX(500);
+		EventHandler<ActionEvent> back = new EventHandler<ActionEvent>() { 
+			public void handle(ActionEvent e) 
+			{ 
+				System.out.println("History button selected"); 
+				state = 0;
+				start(stage);
+			} 
+		}; 
+		backButton.setOnAction(back);
 		if (state==-1) {
 			Group startScreen = new Group();
 			Stop[] stops = {new Stop(0, Color.PINK),new Stop(1, Color.ORANGE)};
@@ -59,8 +71,6 @@ public class Main extends Application {
 			Group introScreen = new Group();
 			Scene introScreen1 = new Scene(introScreen, maxSize, maxSize, Color.BEIGE);
 			stage.setTitle("Welcome!");
-			Circle circle = new Circle(40,40,40);
-			circle.setFill(Color.BLACK);
 			Text text = new Text();
 			text.setText("Hey there! Pick a category below to get started.");
 			text.setX(100); text.setY(100);
@@ -74,7 +84,6 @@ public class Main extends Application {
 			animalButton.setLayoutX(400); animalButton.setLayoutY(600);
 			Button fruitButton = new Button("Fruits");
 			fruitButton.setLayoutX(500); fruitButton.setLayoutY(600);
-      
 			// History category
 			EventHandler<ActionEvent> history_event = new EventHandler<ActionEvent>() { 
 				public void handle(ActionEvent e) 
@@ -123,21 +132,52 @@ public class Main extends Application {
       
 			// Render buttons on intro screen
 			introScreen.getChildren().add(text);
-			introScreen.getChildren().add(circle);
 			introScreen.getChildren().add(celebButton);
 			introScreen.getChildren().add(historyButton);
 			introScreen.getChildren().add(animalButton);
 			introScreen.getChildren().add(fruitButton);
+			introScreen.getChildren().add(backButton);
 			stage.setScene(introScreen1);
 		}
 		
 		if (state==1) {
+			String[] pres_locs = {"file:./Image Revealer Game/src/Images/Presidents/GW-1.png", "file:./Image Revealer Game/src/Images/Presidents/JA-2.png", "file:./Image Revealer Game/src/Images/Presidents/JM-4.png", "file:./Image Revealer Game/src/Images/Presidents/JM-5.png", "file:./Image Revealer Game/src/Images/Presidents/TJ-3.png"};
+			int random_img = (int)(Math.random()*5);
 			Pane root = new Pane();
 			Scene scene = new Scene(root, maxSize, maxSize);
-			new ImageToCircleSplitter("file:./Image Revealer Game/src/Images/koala.png", root, maxSize, targetDepth);
-        
-      stage.setScene(scene);
+			new ImageToCircleSplitter(pres_locs[random_img], root, maxSize, targetDepth);
+			root.getChildren().add(backButton);
+			stage.setScene(scene);
 		}
+		if (state==2) {
+			String[] celeb_locs = {"file:./Image Revealer Game/src/Images/Celebrities/Brad_Pitt-3.jpg", "file:./Image Revealer Game/src/Images/Celebrities/Kevin_Hart-4.png", "file:./Image Revealer Game/src/Images/Celebrities/Lebron_James-5.png", "file:./Image Revealer Game/src/Images/Celebrities/Matthew_Mccounagey-2.png", "file:./Image Revealer Game/src/Images/Celebrities/Robert_Downey_Jr-1.jpg"};
+			int random_img2 = (int)(Math.random()*5);
+			Pane root2 = new Pane();
+			Scene scene2 = new Scene(root2, maxSize, maxSize);
+			new ImageToCircleSplitter(celeb_locs[random_img2], root2, maxSize, targetDepth);
+			root2.getChildren().add(backButton);
+			stage.setScene(scene2);
+		}
+		if (state==3) {
+			String[] animal_locs = {"file:./Image Revealer Game/src/Images/Animals/Eagle-4.png", "file:./Image Revealer Game/src/Images/Animal/Lion-3.png", "file:./Image Revealer Game/src/Images/Animals/Panda-1.png", "file:./Image Revealer Game/src/Images/Animals/Rhino-5.png", "file:./Image Revealer Game/src/Images/Animals/Tiger-2.png"};
+			int random_img3 = (int)(Math.random()*5);
+			Pane root3 = new Pane();
+			Scene scene3 = new Scene(root3, maxSize, maxSize);
+			new ImageToCircleSplitter(animal_locs[random_img3], root3, maxSize, targetDepth);
+			root3.getChildren().add(backButton);
+			stage.setScene(scene3);
+		}
+		if (state==4) {
+			String[] fruit_locs = {"file:./Image Revealer Game/src/Images/Fruits/Apple-1.png", "file:./Image Revealer Game/src/Images/Fruits/Banana-2.png", "file:./Image Revealer Game/src/Images/Fruits/Grape-5.png", "file:./Image Revealer Game/src/Images/Fruits/Kiwi-4.png", "file:./Image Revealer Game/src/Images/Fruits/Orange-3.png"};
+			int random_img4 = (int)(Math.random()*5);
+			Pane root4 = new Pane();
+			Scene scene4 = new Scene(root4, maxSize, maxSize);
+			new ImageToCircleSplitter(fruit_locs[random_img4], root4, maxSize, targetDepth);
+			root4.getChildren().add(backButton);
+			stage.setScene(scene4);
+		}
+        
+		
 		stage.setTitle("Guess Who");
 		stage.show();
 	}
