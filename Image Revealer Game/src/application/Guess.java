90c53e4089b6e5775c367c21bbf989e2182d6f21
ShawnZhuPro <shawnz05827@gmail.com>
2024-05-17 17:31:55 -0700

    Show guess history w/ scrollable window

--- Image Revealer Game/src/application/Guess.java
+++ Image Revealer Game/src/application/Guess.java
@@ -1,15 +1,24 @@
 package application;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class Guess {
 	private static String userGuess;
 	private static String answer;
+	private static VBox guessBox;
+	private static Queue<String> guessHistory;
 
 	public Guess(Pane root, String answer) {
 		Guess.answer = answer;
+		Guess.guessBox = new VBox(5);
+		Guess.guessHistory = new LinkedList<>();
 		initialize(root);
 	}
 
@@ -25,11 +34,18 @@ public class Guess {
         // Checks if guess is correct then clears the field then displays guess on screen then redirects to win screen if guess is correct
         guessField.setOnAction(event -> {
             userGuess = guessField.getText();
+            addGuess(userGuess);
             System.out.println(userGuess); 
             System.out.println(isCorrect());
-            guessField.clear(); 
-            
+            guessField.clear();
         });
+        
+        // Initialize guessBox inside a scrollable pane
+        ScrollPane scrollPane = new ScrollPane(guessBox);
+        scrollPane.setLayoutX(800);
+        scrollPane.setLayoutY(50);
+        scrollPane.setPrefSize(200, 500); 
+        root.getChildren().add(scrollPane);
 	}
 	
 	public static String getUserGuess() {
@@ -44,6 +60,20 @@ public class Guess {
         Text answerText = new Text(500, 200, "Answer: " + answer);
         root.getChildren().add(answerText);
 	}
+	
+	public static void addGuess(String guess) {
+		guessHistory.add(guess);
+		updateGuessBox();
+	}
+	
+	public static void updateGuessBox() {
+        guessBox.getChildren().clear();
+        int guessNumber = 1;
+        for (String guess : guessHistory) {
+            guessBox.getChildren().add(new Text(guessNumber + ". " + guess));
+            guessNumber++;
+        }
+    }
 	
 	public static boolean isCorrect() {
 		return (userGuess.toLowerCase().equals(answer.toLowerCase()));
