7b96752ad729899febfb290ba64242952a163849
Shawn Zhu <1905827@01-19785.pusd.dom>
2024-05-20 11:28:23 -0700

    Highscores class draft

--- Image Revealer Game/src/application/Highscores.java
+++ Image Revealer Game/src/application/Highscores.java
@@ -1,5 +1,52 @@
 package application;
 
+import java.util.Scanner;
+import java.io.File;
+
 public class Highscores {
+	private static String fileName;
+	private static String name;
+	private static int score;
+	private static String category;
+	private static String highscoreName;
+	private static int highscore;
+	private static String highscoreCategory;
 	
+	public Highscores(String fileName, String name, int score, String category) {
+		Highscores.fileName = fileName;
+		Highscores.name = name;
+		Highscores.score = score;
+		Highscores.category = category;
+		initialize();
+	}
+	
+	// Store highscores.csv data into hashmap
+	private static void initialize() {
+		while(true) {
+			try {
+				Scanner scanner = new Scanner(new File(fileName));
+				
+				while(scanner.hasNextLine()) {
+					
+				}
+			}
+		}
+	}
+	
+	public void updateHighscoreName(String name) {
+		Highscores.highscoreName = name;
+	}
+	
+	public void updateHighScore(int score) {
+		Highscores.highscore = score;
+	}
+	
+	public void updateHighScoreCategory(String category) {
+		Highscores.highscoreCategory = category;
+	}
+	
+	// Scoring algorithm implementation
+	public static int getScore() {
+		return 0;
+	}
 }
